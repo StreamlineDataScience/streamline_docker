409e3056da9f9b9dd85d47462624def3073c2315
Your Name <you@example.com>
2022-08-05 21:32:14 +0000

    fixing shiny vm

--- build_shinyvm.R
+++ build_shinyvm.R
@@ -7,7 +7,13 @@ source("docker_functions.R")
 
 
 location = c("us", "us-east4-c", "us-east4")
-pre_steps = NULL
+secret = "ssh-deploy-key"
+x = trailrun::build_setup_ssh()
+pre_steps = c(
+  googleCloudRunner::cr_buildstep_gitsetup(secret),
+  x$pre_steps
+)
+
 
 result = cr_deploy_docker(
   local = "~/streamline_docker",
@@ -15,5 +21,6 @@ result = cr_deploy_docker(
   dockerfile = "~/streamline_docker/dockerfiles/Dockerfile_shinyvm",
   timeout = 3600L,
   pre_steps = pre_steps,
+  post_steps = x$post_steps,
   kaniko_cache = FALSE
 )
